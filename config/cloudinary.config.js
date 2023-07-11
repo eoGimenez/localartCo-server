bdfa584e3475a31d272071c50c364707da331ff6
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-11 20:40:42 +0200

    nuevas extenciones para cloudinary

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,19 +1,19 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_KEY,
+	api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    allowed_formats: ["jpg", "png"],
-    folder: "localartco" 
-  }
+	cloudinary,
+	params: {
+		allowed_formats: ['svg', 'png', 'jpeg', 'jpg'],
+		folder: 'localartco',
+	},
 });
 
 module.exports = multer({ storage });
